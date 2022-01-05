1a543e435941baf959b35bb5ee3f8a4152a18537
zahrahzehak <iqbalstandard@gmail.com>
2022-01-05 16:29:47 -0500

    helo world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("helo world");
     }
 }
